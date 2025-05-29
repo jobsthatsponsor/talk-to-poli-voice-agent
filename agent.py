2b956b9675a14b83af5a633c7e655e8527e425ee
Karan Navani <kknavani@gmail.com>
2025-05-29 12:20:03 +0100

    Comment out room recording setup code

--- agent.py
+++ agent.py
@@ -41,26 +41,27 @@ class Assistant(Agent):
 
 async def entrypoint(ctx: agents.JobContext):
 
-    # Set up recording
-    req = api.RoomCompositeEgressRequest(
-        room_name=ctx.room.name,
-        audio_only=True,
-        file_outputs=[api.EncodedFileOutput(
-            file_type=api.EncodedFileType.OGG,
-            filepath="my-room-test.ogg",
-            s3=api.S3Upload(
-                bucket="recordings",
-                region="eu-west-2",
-                endpoint=os.getenv("S3_SUPABASE_ENDPOINT"),
-                access_key=os.getenv("S3_ACCESS_KEY_ID"),
-                secret=os.getenv("S3_SECRET_ACCESS_KEY"),
-                force_path_style=True
-            ),
-        )],
-    )
+    # # Set up recording
+    # req = api.RoomCompositeEgressRequest(
+    #     room_name=ctx.room.name,
+    #     audio_only=True,
+    #     file_outputs=[api.EncodedFileOutput(
+    #         file_type=api.EncodedFileType.OGG,
+    #         filepath="my-room-test.ogg",
+    #         s3=api.S3Upload(
+    #             bucket="recordings",
+    #             region="eu-west-2",
+    #             endpoint=os.getenv("S3_SUPABASE_ENDPOINT"),
+    #             access_key=os.getenv("S3_ACCESS_KEY_ID"),
+    #             secret=os.getenv("S3_SECRET_ACCESS_KEY"),
+    #             force_path_style=True
+    #         ),
+    #     )],
+    # )
 
-    lkapi = api.LiveKitAPI()
-    await lkapi.egress.start_room_composite_egress(req)
+    # lkapi = api.LiveKitAPI()
+    # await lkapi.egress.start_room_composite_egress(req)
+    # await lkapi.aclose()
 
     async def write_transcript():
         # Initialise Supabase client
@@ -92,7 +93,6 @@ async def entrypoint(ctx: agents.JobContext):
 
     ctx.add_shutdown_callback(write_transcript)
 
-    await lkapi.aclose()
     session = AgentSession(
         llm=openai.realtime.RealtimeModel.with_azure(
             voice="shimmer",
